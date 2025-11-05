2defe26903a9dddf549664228a334bbaebe138a5
Collins Kiplimo <ckiplimo54@gmail.com>
2025-11-05 03:35:59 +0300

    sliding window algos

--- src/main/java/com/collicode/algodatastructure/leetcode/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/collicode/algodatastructure/leetcode/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public int longestSubstringWithoutRepeatingCharacters(String s) {
-        Map<Character, Integer> seen = new HashMap<>();
-        int max_length = 0;
+        Map<Character, Integer> state = new HashMap<>();
+        int maxLength = 0;
         int start = 0;
 
         if (s == null) {
@@ -14,23 +14,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
         }
 
         for (int end = 0; end < s.length(); end++) {
-            char c = s.charAt(end);
 
-            // Increment count of current character
-            seen.put(c, seen.getOrDefault(c, 0) + 1);
 
-            // If there's a duplicate, shrink window from the left
-            while (seen.get(c) > 1) {
-                char startChar = s.charAt(start);
-                seen.put(startChar, seen.get(startChar) - 1);
-                start++;
+            if (state.containsKey(s.charAt(end))) {
+                start = Math.max(start, state.get(s.charAt(end)) + 1);
             }
+            state.put(s.charAt(end), end);
 
             // Update maximum length (add +1 to include both ends)
-            max_length = Math.max(max_length, end - start + 1);
+            maxLength = Math.max(maxLength, end - start + 1);
         }
 
-        return max_length;
+        return maxLength;
 
     }
 }
