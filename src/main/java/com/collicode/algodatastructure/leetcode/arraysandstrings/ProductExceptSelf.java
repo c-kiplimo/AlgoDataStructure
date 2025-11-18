73cdba64ca5bd1366e7a7d95ed5700ff00950652
Collins Kiplimo <ckiplimo54@gmail.com>
2025-11-19 01:36:28 +0300

    feat:handle dsas

---
+++ src/main/java/com/collicode/algodatastructure/leetcode/arraysandstrings/ProductExceptSelf.java
@@ -0,0 +1,24 @@
+package com.collicode.algodatastructure.leetcode.arraysandstrings;
+
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        // Left products
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        // Right products
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right *= nums[i];
+        }
+
+        return res;
+    }
+
+}
